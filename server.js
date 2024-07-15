2cbaeec0c7714fea204d6a9d2683ae7f8552d702
ttangzzi <cvbg0802@gmail.com>
2024-07-15 20:04:58 +0900

    passport 라이브러리

--- server.js
+++ server.js
@@ -28,6 +28,20 @@ new MongoClient(url)
     console.log(err);
   });
 
+// passport 라이브러리 코드
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+
+app.use(passport.initialize())
+app.use(session({
+  secret: 'abc', // 암호화에 쓸 비번
+  resave : false,
+  saveUninitialized : false
+}))
+
+app.use(passport.session())
+
 // ==================================== //
 app.listen(8081, () => {
   console.log("http://localhost:8081 에서 서버 실행 중");
@@ -159,3 +173,34 @@ app.get("/list/:id", async (요청, 응답) => {
     .toArray();
   응답.render("list.ejs", { posts: result, pages: page });
 });
+
+app.get('/login', (요청, 응답)=> {
+  응답.render('login.ejs');
+});
+
+app.post('/login', (요청, 응답)=>{
+  passport.authenticate('local', (error, user, info)=> {
+    if (error) return 응답.status(500).json(error);
+    if (!user) return 응답.status(401).json(info.message);
+    요청.logIn(user, (err)=> {
+      if (err) return next(err);
+      응답.redirect('/');
+    })
+  })(요청, 응답, next);
+})
+
+passport.use(new LocalStrategy(async (입력한아이디, 입력한비번, cb)=> {
+  try {
+    let result = await db.collection('user').findOne({ username : 입력한아이디})
+    if (!result) {
+      return cb(null, false, { message: '아이디 DB에 없음'});
+    }
+    if (result.password == 입력한비번) {
+      return cb(null, result);
+    } else {
+      return cb(null, false, { message : '비번불일치'});
+    }
+  } catch(e) {
+    console.log(e);
+  }
+}))
